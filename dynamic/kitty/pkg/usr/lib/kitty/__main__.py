510a20ef7318bcbf06e0a46efbe5918240fa6564
kosakovitch-2013 <kosavinna@gmail.com>
2026-09-25 20:39:42 +0300

    Add alacritty, removed kitty cuz i hate python

--- dynamic/kitty/pkg/usr/lib/kitty/__main__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# License: GPL v3 Copyright: 2015, Kovid Goyal <kovid at kovidgoyal.net>
-
-
-if __name__ == '__main__':
-    from kitty.entry_points import main
-    main()
--- dynamic/kitty/pkg/usr/lib/kitty/kittens/choose_files/__init__.py
+++
@@ -1,6 +0,0 @@
-def syntax_aliases(x: str) -> dict[str, str]:
-    ans = {}
-    for x in x.split():
-        k, _, v = x.partition(':')
-        ans[k] = v
-    return ans
